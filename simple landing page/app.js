40b44288c162293df69f6c8a9604e2348e12cf32
phoenix-vylor <phoenixvylor@gmail.com>
2024-07-23 09:32:52 +0530

    Add files via upload

---
+++ simple landing page/app.js
@@ -0,0 +1,7 @@
+const trans = document.getElementById("trans")
+const navlist = document.getElementById("navlist")
+
+trans.addEventListener("click" , ()=>{
+    
+    navlist.classList.toggle("navlist-active")
+})
